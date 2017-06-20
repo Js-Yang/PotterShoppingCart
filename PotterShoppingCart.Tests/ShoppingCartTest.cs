6b7971382e86e4359b89ca4629263de68d47714a
jason.yang <chyang590@gmail.com>
2017-06-20 22:41:22 +0800

    add test case for scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190, and the test case fail

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -13,7 +13,21 @@ namespace PotterShoppingCart.Tests
             var expected = 100;
 
             //Act
-            var actaul = shoppingCart.CalculateFee();
+            var actaul = shoppingCart.CalculateFee(1, 0);
+
+            //Assert
+            Assert.AreEqual(expected, actaul);
+        }
+
+        [Test]
+        public void CalculateFee_Buy_A_First_Episode_And_Second_Episode_Should_Be_190()
+        {
+            //Arrange
+            var shoppingCart = new ShoppingCart();
+            var expected = 190;
+
+            //Act
+            var actaul = shoppingCart.CalculateFee(1, 1);
 
             //Assert
             Assert.AreEqual(expected, actaul);
@@ -22,9 +36,14 @@ namespace PotterShoppingCart.Tests
 
     public class ShoppingCart
     {
-        public double CalculateFee()
+        public double CalculateFee(int firstEpisodeCount, int secondEpisodeCount)
         {
-            return 100;
+            var fee = 0;
+            if (firstEpisodeCount == 1)
+            {
+                fee = 100;
+            }
+            return fee;
         }
     }
 }
