012243733d867b8e9369934203af4b8bb7761ca4
jason.yang <chyang590@gmail.com>
2017-06-21 00:07:09 +0800

    add test case Scenario: 一次買了整套，一二三四五集各買了一本，價格應為100*5*0.75=375, and the test case fail

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -76,6 +76,27 @@ namespace PotterShoppingCart.Tests
             //Assert
             Assert.AreEqual(expected, actaul);
         }
+
+
+        [Test]
+        public void CalculateFee_Buy_A_First_Episode_A_Second_Episode_A_ThirdEpisode_A_FourEpisode_A_FifthEpisode_Should_Be_375()
+        {
+            //Arrange
+            var shoppingCart = new ShoppingCart();
+            var expected = 375;
+
+            shoppingCart.AddProduct(1, 1);
+            shoppingCart.AddProduct(2, 1);
+            shoppingCart.AddProduct(3, 1);
+            shoppingCart.AddProduct(4, 1);
+            shoppingCart.AddProduct(5, 1);
+
+            //Act
+            var actaul = shoppingCart.CalculateFee();
+
+            //Assert
+            Assert.AreEqual(expected, actaul);
+        }
     }
 
     public class ShoppingCart
