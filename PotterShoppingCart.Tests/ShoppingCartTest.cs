3442151bc6edc6bca1496a1a8b6c9abb265783b3
jason.yang <chyang590@gmail.com>
2017-06-20 21:57:29 +0800

    add first test for calculate fee of shopping cart and the test case fail

---
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestFixture]
+    public class ShoppingCartTest
+    {
+        [Test]
+        public void CalculateFee_Buy_A_First_Episode_Should_Be_100()
+        {
+            //Arrange
+            var shoppingCart = new ShoppingCart();
+            var expected = 100;
+
+            //Act
+            var actaul = shoppingCart.CalculateFee();
+
+            //Assert
+            Assert.AreEqual(expected, actaul);
+        }
+    }
+
+    public class ShoppingCart
+    {
+        public double CalculateFee()
+        {
+            return 0;
+        }
+    }
+}
