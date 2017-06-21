df1f6ef651a831f6e3ce3af4a778867007bc2634
jason.yang <chyang590@gmail.com>
2017-06-21 15:51:05 +0800

    shoppingCart Class did not commit, upload it

---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        private Dictionary<int, int> products = new Dictionary<int, int>();
+
+        public void AddProduct(int productNumber, int count)
+        {
+            products.Add(productNumber, count);
+        }
+
+        public Dictionary<int, int> GetContent()
+        {
+            return products;
+        }
+    }
+}
