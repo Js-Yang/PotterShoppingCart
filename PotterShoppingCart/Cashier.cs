ba214c6b0108cdd1eb4676e0eb0e803de9681b1a
jason.yang <chyang590@gmail.com>
2017-06-21 15:10:11 +0800

    rename from SetDicountDefinition to GetDicountDefinition

---
+++ PotterShoppingCart/Cashier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PotterShoppingCart
+{
+    public class Cashier
+    {
+        private Dictionary<int, double> discountRule;
+
+        public Cashier(Dictionary<int, double> dicountRule)
+        {
+            this.discountRule = dicountRule;
+        }
+
+        public double CalculateFee(Dictionary<int, int> products)
+        {
+            var basePrize = 100;
+            var totalFee = 0d;
+            for (int i = 1; i <= products.Keys.Max(); i++)
+            {
+                var setsCount = products.Count(product => product.Value >= i);
+                if (setsCount == 0)
+                    break;
+                totalFee += setsCount * basePrize * discountRule[setsCount];
+            }
+            return totalFee;
+        }
+    }
+}
